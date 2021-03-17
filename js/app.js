19b966c87d040fc740384b3356b43936e23422ec
Emsad <72226104+emsad87@users.noreply.github.com>
2021-03-18 00:37:51 +0100

    Update
    - Added colors, codebox, upgraded hero, added new controllers flex, gap. position etc
    - Shorter class names

---
+++ js/app.js
@@ -0,0 +1,14 @@
+let mainNav = document.querySelector("#js-menu");
+let navBarToggle = document.querySelector("#js-navbar-toggle");
+
+let codebox = document.querySelectorAll(".code");
+
+navBarToggle.addEventListener("click", function () {
+  mainNav.classList.toggle("active");
+});
+
+codebox.forEach((element) => {
+  element.style.height = "1px";
+  element.style.height = element.scrollHeight + "px";
+  element.readOnly = true;
+});
